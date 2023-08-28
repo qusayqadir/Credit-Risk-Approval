2a4920f6ad4e4b63bd9636d60bccaf7887da1961
qusayqadir <qusayqadir78@gmail.com>
2023-08-28 17:58:55 -0400

    learning to describe the data frames and determine what to filter and end goal: learn how to replace strings with integers

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
-from sklearn.cross_decomposition import KFold
+from sklearn.model_selection import KFold
 from sklearn.linear_model import LogisticRegression
 
 
+def main():
+    clean_data = pd.read_csv('Data-Set/train.csv')
+    #print(clean_data)
+    #print(clean_data.head())
+    #print(clean_data.isna().sum())
+    #print(clean_data.describe())
+    #print(clean_data[clean_data.Credit_History ])
+    print(clean_data["Credit_History"].value_counts())
+    print(clean_data["Property_Area"].value_counts())
 
-def clean_data():
-    data_frame = pd.read_csv('Data-Set/cs-test.csv')
 
-
-
-clean_data
+main()
