dc88ea4ed2c851fcc14309cc9d0556952c912674
qusayqadir <qusayqadir78@gmail.com>
2023-08-29 17:19:16 -0400

    final clean data set, and trained model

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import matplotlib as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn import preprocessing
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import KFold
 from sklearn.linear_model import LogisticRegression
 
 def values_in_dataset(df):
@@ -100,8 +97,13 @@ def main():
 
     df['Total_Income'] = df['ApplicantIncome'] + df['CoapplicantIncome']
     print(df)
-
-def logistic_regression():
-    predicators = ['Dependents', 'Education','Credit History', 'Total_Income' ]
+    X = df.drop('Loan_Status', axis = 1)
+    y = df['Loan_Status']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print("The Accuracy of the model is:", accuracy)
 
 main()
